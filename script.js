83596566c3577030d678d0ad9ce54f30cf5905ea
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-15 09:29:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // This is the boilerplate code given for you
 // You can modify this code
+
 // Product data
 const products = [
   { id: 1, name: "Product 1", price: 10 },
@@ -11,27 +12,110 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+
+// Get cart from sessionStorage
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+
+    li.innerHTML = `
+      ${product.name} - $${product.price}
+      <button class="add-to-cart-btn" data-id="${product.id}">
+        Add to Cart
+      </button>
+    `;
+
     productList.appendChild(li);
   });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  cartList.innerHTML = "";
+
+  cart.forEach((product) => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      ${product.name} - $${product.price}
+      <button class="remove-from-cart-btn" data-id="${product.id}">
+        Remove
+      </button>
+    `;
+
+    cartList.appendChild(li);
+  });
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+  const product = products.find(
+    (product) => product.id === productId
+  );
+
+  if (product) {
+    cart.push(product);
+
+    sessionStorage.setItem(
+      "cart",
+      JSON.stringify(cart)
+    );
+
+    renderCart();
+  }
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+  cart = cart.filter(
+    (product) => product.id !== productId
+  );
+
+  sessionStorage.setItem(
+    "cart",
+    JSON.stringify(cart)
+  );
+
+  renderCart();
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+  cart = [];
+
+  sessionStorage.setItem(
+    "cart",
+    JSON.stringify(cart)
+  );
+
+  renderCart();
+}
+
+// Add to cart button
+productList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-to-cart-btn")) {
+    const productId = Number(event.target.dataset.id);
+
+    addToCart(productId);
+  }
+});
+
+// Remove from cart button
+cartList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const productId = Number(event.target.dataset.id);
+
+    removeFromCart(productId);
+  }
+});
+
+// Clear cart button
+clearCartBtn.addEventListener("click", clearCart);
 
 // Initial render
 renderProducts();
